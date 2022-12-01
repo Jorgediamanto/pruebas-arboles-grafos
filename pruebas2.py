b4bb485e98c0638d85339fe35ce7c58569e079c3
Jorge <jorgediamanto01@gmail.com>
2022-12-01 19:30:08 +0100

    as

--- pruebas2.py
+++ pruebas2.py
@@ -1,1 +1,33 @@
-print("hola")
+class TreeNode:
+
+    def __init__(self,data,children=[]):
+        self.data = data
+        self.children = children
+
+    def __str__(self,level=0):
+        ret = " "*level + str(self.data) +"\n"
+        for children in self.children:
+            
+        return ret
+
+    def addChild(self,Treenode):
+        self.children.append(Treenode)
+
+        
+tree = TreeNode("Drinks",[])
+print(tree)
+cold = TreeNode("cold",[])
+hot = TreeNode("hot",[])
+tree.addChild(hot)
+tree.addChild(cold)
+
+coffee = TreeNode("coffee",[])
+tea = TreeNode("tea",[])
+hot.addChild(coffee)
+hot.addChild(tea)
+
+cola = TreeNode("cola",[])
+fanta = TreeNode("fanta",[])
+cold.addChild(cola)
+cold.addChild(fanta)
+print(tree)
