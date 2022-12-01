be09573763e34cce9b006cb98de437d89f83cb3a
Jorge <jorgediamanto01@gmail.com>
2022-12-01 18:58:12 +0100

    asa

--- pruebas.py
+++ pruebas.py
@@ -16,8 +16,12 @@ class TreeNode:
     def addChild(self,Treenode):
         self.children.append(Treenode)
 
+        
 tree = TreeNode("Drinks",[])
 print(tree)
 cold = TreeNode("cold",[])
 hot = TreeNode("hot",[])
-tree
+tree.addChild(hot)
+tree.addChild(cold)
+
+print(tree)
